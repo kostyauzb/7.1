e70b603cbc6d5b4b08ebe81c6512d7d66b0320a2
kostyauzb <qosimjonomonovko@gmail.com>
2024-02-09 01:21:29 +0500

    fixedSecondTime

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+const images = [
+  'images/img_1.jpg',
+  'images/img_2.jpg',
+  'images/img_3.webp',
+  'images/img_4.webp',
+  'images/img_5.webp'
+];
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <App images={images} />
   </React.StrictMode>
 );
