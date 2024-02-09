7c2e23c0db57ef64c17c1990232a4c18cf77b557
kostyauzb <qosimjonomonovko@gmail.com>
2024-02-09 10:41:31 +0500

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,57 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const App = ({ images }) => {
-    const [displayedImage, setDisplayedImage] = useState(images[0]);
+const App = () => {
+  const [displayedImage, setDisplayedImage] = useState("");
+  const images = [
+    "src/images/img_1.jpg",
+    "src/images/img_2.jpg",
+    "src/images/img_3.webp",
+    "src/images/img_4.webp",
+    "src/images/img_5.webp",
+    "src/images/img_6.webp",
+  ];
 
-    const handleImageClick = (src) => {
-        setDisplayedImage(src);
-    };
+  const handleImageClick = (src) => {
+    setDisplayedImage(src);
+  };
 
-    const handleNextImage = () => {
-        const currentIndex = images.findIndex(image => image === displayedImage);
-        const nextIndex = (currentIndex + 1) % images.length;
-        setDisplayedImage(images[nextIndex]);
-    };
+  const handleNextImage = () => {
+    const currentIndex = images.findIndex((image) => image === displayedImage);
+    const nextIndex = (currentIndex + 1) % images.length;
+    setDisplayedImage(images[nextIndex]);
+  };
 
-    const handlePrevImage = () => {
-        const currentIndex = images.findIndex(image => image === displayedImage);
-        const prevIndex = (currentIndex - 1 + images.length) % images.length;
-        setDisplayedImage(images[prevIndex]);
-    };
+  const handlePrevImage = () => {
+    const currentIndex = images.findIndex((image) => image === displayedImage);
+    const prevIndex = (currentIndex - 1 + images.length) % images.length;
+    setDisplayedImage(images[prevIndex]);
+  };
 
-    return (
-        <div className="container" style={{ overflow: 'auto' }}>
-            <div className="opshi_cart">
-                <div className="katta_rasm">
-                    {/* ///////////////////////////////////////////// katta rasm && btn */}
-                    <div className="prev btn" onClick={handlePrevImage}>{`<`}</div>
-                    <img src={displayedImage} alt="Displayed" />
-                    <div className="next btn" onClick={handleNextImage}>{`>`}</div>
-                </div>
-                <div className="kischik_rasm">
-                    {/* ///////////////////////////////////////////////// past kichik rasmlar */}
-                    {images.map((image, index) => (
-                        <button className="btn_img" key={index}>
-                            <img className="btn_img" src={image} alt={`Image ${index}`} onClick={() => handleImageClick(image)} />
-                        </button>
-                    ))}
-                </div>
-            </div>
+  return (
+    <div className="container">
+      <div className="opshi_cart">
+        <div className="katta_rasm">
+          {/* ///////////////////////////////////////////// katta rasm && btn */}
+          <div className="prev btn" onClick={handlePrevImage}>{`<`}</div>
+          <img src={displayedImage || images[0]} alt="Displayed" />
+          <div className="next btn" onClick={handleNextImage}>{`>`}</div>
         </div>
-    );
+        <div className="kischik_rasm">
+          {/* ///////////////////////////////////////////////// past kichik rasmlar */}
+          {images.map((image, index) => (
+            <button className="btn_img" key={index}>
+              <img
+                className="btn_img"
+                src={image}
+                alt={`Image ${index}`}
+                onClick={() => handleImageClick(image)}
+              />
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-const images = [
-  'images/img_1.jpg',
-  'images/img_2.jpg',
-  'images/img_3.webp',
-  'images/img_4.webp',
-  'images/img_5.webp'
-];
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App images={images} />
+    <App />
   </React.StrictMode>
 );
